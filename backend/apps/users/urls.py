c6e815d3ea05c46918a2baf97381b91ea03ee4b1
PropainAcessories <fieldmarshalbonespur69@gmail.com>
2026-05-02 12:23:58 -0500

    forgot a save

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("me/", CurrentUserView.as_view(), name="me"),
-    path("me/update/", ProfileUpdateView.as_view(), name="me-update"),
 ]
