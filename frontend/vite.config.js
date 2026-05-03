834987737299eedb01f322929cebe758f04b04a5
PropainAcessories <fieldmarshalbonespur69@gmail.com>
2026-05-03 10:58:33 -0500

    forgot /static/

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 // CHANGE TO STATIC BEFORE GOING INTO PRODUCTION!!!!!!!!!
 export default defineConfig({
-  base: "/",
+  base: "/static/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
